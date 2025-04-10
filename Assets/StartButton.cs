53ef24e64828aabedb1892d8864ae960a1f1a346
renzoboyy <fatdonut380@gmail.com>
2025-04-11 01:29:05 +0800

    F

--- Assets/StartButton.cs
+++ Assets/StartButton.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
     void OnMouseDown()
     {
-        // This runs when the GameObject is clicked
-        Debug.Log("Clicked on " + gameObject.name);
-        // Add your event here
+        SceneManager.LoadSceneAsync("CharacterSelection");
     }
 }
