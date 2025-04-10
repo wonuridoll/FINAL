af57e0e67b49311b3bb77d61556dfa21d256e3d6
renzoboyy <fatdonut380@gmail.com>
2025-04-10 14:16:55 +0800

    j

---
+++ Assets/Scenes/SecondMapTransition.cs
@@ -0,0 +1,35 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class SecondMapTransition : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D mapBoundary;
+    [SerializeField] private GameObject mapSpawnPoint; // New field for spawn point
+    [SerializeField] private Direction direction;
+
+    private CinemachineConfiner2D confiner;
+
+    private enum Direction { Up, Down, Left, Right }
+
+    private void Awake()
+    {
+        confiner = FindFirstObjectByType<CinemachineConfiner2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundary;
+
+            if (mapSpawnPoint != null)
+            {
+                collision.gameObject.transform.position = mapSpawnPoint.transform.position;
+            }
+            else
+            {
+                Debug.LogWarning("Map spawn point is not assigned in the inspector!");
+            }
+        }
+    }
+}
